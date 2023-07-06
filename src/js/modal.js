de5742903f69e773aa9c8f11e4423f32c49cba78
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-07-06 19:51:26 +0300

    finish

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,6 +4,7 @@
     openModalBtn1: document.querySelector("[data-modal-open-1]"),
     openModalBtn2: document.querySelector("[data-modal-open-2]"),
     openModalBtn3: document.querySelector("[data-modal-open-3]"),
+    openModalBtn4: document.querySelector("[data-modal-open-4]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
@@ -11,6 +12,7 @@
   refs.openModalBtn1.addEventListener("click", toggleModal);
   refs.openModalBtn2.addEventListener("click", toggleModal);
   refs.openModalBtn3.addEventListener("click", toggleModal);
+  refs.openModalBtn4.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
