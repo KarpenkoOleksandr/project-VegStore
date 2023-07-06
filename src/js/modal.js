64dafdb01c12ac8e3361e9f8dab2016c0202dec3
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-07-06 19:36:16 +0300

    skript

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,74 +1,18 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     openModalBtn: document.querySelector('[data-modal-open1]'),
-//     openModalBtn: document.querySelector('[data-modal-open2]'),
-//     openModalBtn: document.querySelector('[data-modal-open3]'),
-//     penModalBtn: document.querySelector('[data-modal-open4]'),
-//     penModalBtn: document.querySelector('[data-modal-open5]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//     document.body.classList.toggle('no-scroll');
-//   }
-// })();
-
-document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-        // прописуємо дата атрибути, в залежності від кільності модальних вікон, задаємо на backdrop
-        const modals = [
-            "data-first-modal",
-            // "data-second-modal",
-            // "data-third-modal",
-        ];
-
-
-        modals.forEach((element) => {
-            const openModalSelector = element + "-open";
-            const closeModalSelector = element + "-close";
-
-            const openModalBtns = document.querySelectorAll(`[${openModalSelector}]`);
-            const closeModalBtns = document.querySelectorAll(
-                `[${closeModalSelector}]`
-            );
-            const modal = document.querySelector(`[${element}]`);
-
-            if (!modal) logModalError("Can`t find Modal with attribute " + modal);
-            if (openModalBtns.length === 0)
-                logModalError(
-                    "Can`t find Open modal button with attribute " + openModalSelector
-                );
-            if (closeModalBtns.length === 0)
-                logModalError(
-                    "Can`t find Close modal button with attribute " + closeModalSelector
-                );
-            if (!modal || openModalBtns.length === 0 || closeModalBtns.length === 0)
-                return;
-
-            openModalBtns.forEach((openBtn) =>
-                openBtn.addEventListener("click", toggleModal)
-            );
-            closeModalBtns.forEach((closeBtn) =>
-                closeBtn.addEventListener("click", toggleModal)
-            );
-
-            function toggleModal() {
-                document.body.classList.toggle("modal-open");
-                modal.classList.toggle("is-hidden");
-            }
-        });
-    },
-    false
-);
-
-function logModalError(text) {
-    const styles = "color: #bada55";
-    console.log("%c" + text, styles);
-}
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn1: document.querySelector("[data-modal-open-1]"),
+    openModalBtn2: document.querySelector("[data-modal-open-2]"),
+    openModalBtn3: document.querySelector("[data-modal-open-3]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
